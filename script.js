e740101cc21f96b7a96c0e793b0082b1ec5814f9
v-angelo <v.angelo003@gmail.com>
2026-02-12 10:41:10 +0530

    added session storage functionality

--- script.js
+++ script.js
@@ -5,30 +5,10 @@ const form = document.getElementById('userForm');
 const tableBody = document.getElementById('tableBody');
 const editIndexInput = document.getElementById('editIndex');
 
-// add entry to users
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    // get name & email
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    
-    if (editIndexInput.value == "") {
-        // add name & email to users
-        users.push({ name, email });
-    } else {
-        // edit user
-        users[editIndexInput.value] = { name, email };
-        editIndexInput.value = "";
-    }
-    
-    form.reset();
-    console.log(users);
-    displayUsers();
-});
-
 // display users inside table body
 const displayUsers = () => {
     tableBody.innerHTML = "";
+    let users = JSON.parse(sessionStorage.getItem("users"));
     users.forEach((user, index) => {
         tableBody.innerHTML += `
         <tr>
@@ -46,6 +26,35 @@ const displayUsers = () => {
     });
 }
 
+if (sessionStorage.getItem("users")) {
+    users = JSON.parse(sessionStorage.getItem("users"));
+    displayUsers();
+} else {
+
+}
+
+// add entry to users
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // get name & email
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    
+    if (editIndexInput.value == "") {
+        // add name & email to users
+        users.push({ name, email });
+    } else {
+        // edit user
+        users[editIndexInput.value] = { name, email };
+        editIndexInput.value = "";
+    }
+
+    form.reset();
+    console.log(users);
+    sessionStorage.setItem("users", JSON.stringify(users));
+    displayUsers();
+});
+
 // used to edit particular user
 const editUser = (index) => {
     document.getElementById('name').value = users[index].name;
@@ -57,6 +66,8 @@ const editUser = (index) => {
 const deleteUser = (index) => {
     if (confirm("Are you sure, do you want to delete the data?")) {
         users.splice(index, 1);
+        // insert users into storage
+        sessionStorage.setItem("users", JSON.stringify(users));
         displayUsers();
     }
 }
